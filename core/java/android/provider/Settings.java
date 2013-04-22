0929652d64796abb2f4e993700954a212315ba00
blk_jack <jcorcoran+github@gmail.com>
2013-04-22 08:50:13 +0800

    MediaScanner behavior on boot (1/3) Lets you enable/disable/ask (with a notification) media scanning on boot. Can speed up boot and reduce initial boot lag from MediaScanner
    Change-Id: I634ae96b1dcb493b966b4653589cd97523b63da7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4181,6 +4181,15 @@ public final class Settings {
          */
         public static final String RECENTS_RAM_BAR_ACTIVE_APPS_COLOR = "recents_ram_bar_active_apps_color";
 
+        /**
+         * MediaScanner behavior on boot.
+         * 0 = enabled
+         * 1 = ask (notification)
+         * 2 = disabled
+         * @hide
+         */
+        public static final String MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
+
     }
 
     /**
